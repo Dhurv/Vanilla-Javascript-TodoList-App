1aa898fab1a52eec53ccc94766ed51fd96b502c9
Dhruba Jyoti Bharali <official.dhruvb@gmail.com>
2022-12-07 14:40:57 +0530

    add code for localStorage

--- script.js
+++ script.js
@@ -9,10 +9,24 @@ todoAddbutton.addEventListener('click', addTodo);
 ulTodoList.addEventListener('click', deleteTodo);
 ulTodoList.addEventListener('dblclick', marked);
 
-//Function
+//loading all todos on page load for first time or refresh
+document.addEventListener('DOMContentLoaded', () => {
+    
+    var todos = JSON.parse(localStorage.getItem('todo'))
+    console.log(todos)
+    todos.forEach(todo => {
+        createTodoComponent(todo)
+    });
+})
 
+
+
+//Function
+var todoList = []
 function addTodo(){
     const value = todo.value;
+    todoList.push(value)
+    localStorage.setItem('todo', JSON.stringify(todoList))
     createTodoComponent(value)
     todo.value = '';
 }
@@ -33,18 +47,23 @@ function createTodoComponent(todoValue) {
     editButton.innerText = 'Edit';
     div.appendChild(editButton)
     ulTodoList.appendChild(div)
+
 }
 
 function deleteTodo(e) {
     const itemClicked = e.target;
     if(itemClicked.classList[0] === "deleteTodoList"){
         const item = e.target.parentElement;
+        let todoItem = item.children[0].innerText;
+        deleteTodoFromLocal(todoItem);
         item.remove();
     }
 
     if(itemClicked.classList[0] === "editTodoList"){
         editTodo(e)
         const item = e.target.parentElement;
+        let todoItem = item.children[0].innerText;
+        deleteTodoFromLocal(todoItem);
         item.remove();
     }
 }
@@ -58,4 +77,13 @@ function marked(e) {
     const value = e.target;
     value.style.textDecoration = "line-through";
     value.style.opacity = "0.5"
+}
+
+function deleteTodoFromLocal(todo){
+    if(localStorage.getItem('todo') !== null){
+        todoList = JSON.parse(localStorage.getItem('todo'))
+    }
+    let index = todoList.indexOf(todo)
+    todoList.splice(index, 1);
+    localStorage.setItem('todo', JSON.stringify(todoList))
 }
